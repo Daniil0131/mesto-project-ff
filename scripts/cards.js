87a33fcedce5b53f0980bb644bc176f4fe1f718b
Daniil Nikiforov <Daniil01413@yandex.ru>
2025-05-25 16:52:24 +0300

    Добавил скрипт на добавления картинок и их удаления

--- scripts/cards.js
+++ scripts/cards.js
@@ -22,5 +22,9 @@ const initialCards = [
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    },
+    {
+      name: "Тель-Авив",
+      link: "https://images.unsplash.com/photo-1500990702037-7620ccb6a60a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,37 @@
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
-
+const ContainerPicture = document.querySelector('.places__list')
 // @todo: Функция создания карточки
+function AddPictures(PictureData, DeletePicture) {
+  const PictureTemplate = document.querySelector('#card-template').content;
+  const PictureElement = PictureTemplate.querySelector('.card').cloneNode(true);
+
+  const AddImage = PictureElement.querySelector('.card__image');
+  const AddTitle = PictureElement.querySelector('.card__title');
+  const DeleteButton = PictureElement.querySelector('.card__delete-button');
+
+  AddImage.src = PictureData.link;
+  AddImage.alt = PictureData.name;
+  AddTitle.textContent = PictureData.name;
+
+
+  DeleteButton.addEventListener('click', () => DeletePicture(PictureElement));
+
+  return PictureElement;
+}
+
+// Функция удаления карточки
+function DeletePicture(PictureElement) {
+  PictureElement.remove();
+}
+
+initialCards.forEach(PictureData => {
+  const Picture = AddPictures(PictureData, DeletePicture);
+  ContainerPicture.append(Picture);
+});
+
+// Добавление новой карточки при клике на кнопку
 
 // @todo: Функция удаления карточки
 
